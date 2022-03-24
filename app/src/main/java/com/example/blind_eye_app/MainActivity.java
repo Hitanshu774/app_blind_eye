0b80eee323b9ad8409e89f7bcaedfb1441350d58
Hitanshu <hitanshupatil143@gmail.com>
2022-03-24 15:27:18 +0530

    firebase connected

--- app/src/main/java/com/example/blind_eye_app/MainActivity.java
+++ app/src/main/java/com/example/blind_eye_app/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button login;
+    private TextView email;
+    private TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "I am the toast", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "I am the toast", Toast.LENGTH_SHORT).show();
             }
         });
     }
