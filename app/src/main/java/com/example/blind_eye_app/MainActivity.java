1011c40b864e49d30e0553f058aa46b675129725
Hitanshu <hitanshupatil143@gmail.com>
2022-03-25 16:06:21 +0530

    facing login problem

--- app/src/main/java/com/example/blind_eye_app/MainActivity.java
+++ app/src/main/java/com/example/blind_eye_app/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     private Button login, register;
     private TextView email;
     private TextView password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,16 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "I am the toast", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "I am the toast", Toast.LENGTH_SHORT).show();
+                String email1 = email.getText().toString();
+                String password1 = password.getText().toString();
+
+                if(TextUtils.isEmpty(email1) || TextUtils.isEmpty(password1)){
+                    Toast.makeText(MainActivity.this, "Enter email and password!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    login(email1, password1);
+                }
             }
         });
 
@@ -35,6 +50,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, register_page.class));
+            }
+        });
+    }
+    private void login(String email, String password){
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(MainActivity.this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                //we can link next page through an intent.
+                Toast.makeText(MainActivity.this, "Correct password!!!!", Toast.LENGTH_SHORT).show();
             }
         });
     }
